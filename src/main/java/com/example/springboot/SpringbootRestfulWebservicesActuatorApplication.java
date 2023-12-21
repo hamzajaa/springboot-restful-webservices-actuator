7e82b377472d687ad5fdb1066d7a396a960fca3f
HAMZA JAA <hamzajaa2017@gmail.com>
2023-12-21 21:32:59 +0100

    Initial commit

---
+++ src/main/java/com/example/springboot/SpringbootRestfulWebservicesActuatorApplication.java
@@ -0,0 +1,20 @@
+package com.example.springboot;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootRestfulWebservicesActuatorApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebservicesActuatorApplication.class, args);
+	}
+
+}
